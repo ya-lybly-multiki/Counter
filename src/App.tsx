71c9bd98f6545f000d891e2a255c9e95fda92374
ya-lybly-multiki <trifonov_artem@icloud.com>
2021-12-26 20:14:07 +0300

    counter-Exam

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
+import {SettingsBlock} from "./components/Settings/SettingsBlock";
+import {CounterBlock} from "./components/Counter/CounterBlock";
+import {Button} from "./components/Button/Button";
 import './App.css';
-import {Counter} from "./Counter";
-import {Settings} from "./Settings/Settings";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "./state/store";
+import {increaseValueAC, setCounterValueAC} from "./state/counterReducer";
+import {setError, setSettingsMode} from "./state/settingsReducer";
+import {Dispatch} from "redux";
 
 
 function App() {
-    const [startValue, setStartValue] = useState<number>(0)
-    const [finishValue, setFinishValue] = useState<number>(0)
-    const [counter, setCounter] = useState<number>(startValue)
-    const [error, setError] = useState<string | null>(null)
-    const [enter, setEnter] = useState<string | null>(null)
-    const [disabled, setDisabled] = useState<boolean>(false)
-    const [edit, setEdit] = useState<boolean>(false)
 
+    let dispatch = useDispatch<Dispatch>()
+    let startValue = useSelector<AppRootStateType, number>(state => state.counter.startValue)
+    let maxValue = useSelector<AppRootStateType, number>(state => state.counter.maxValue)
 
 
     useEffect(() => {
-        if (startValue === 0 && finishValue === 0) {
-            setEdit(true)
-            setEnter('enter value and press "set"')
+
+        if (startValue >= maxValue || startValue < 0) {
+            dispatch(setError(true))
+        } else {
+            dispatch(setError(false))
         }
+    }, [startValue, maxValue])
 
-    },[])
-
-    useEffect(() => {
-
-        if (startValue < 0 || startValue > finishValue || finishValue < 0 || (startValue>0 && finishValue>0 && startValue === finishValue)) {
-            setError('Incorrect value')
-            setEdit(true)
-        } else if (startValue > 0 || startValue < finishValue || finishValue > 0) {
-            setError(null)
-            setEdit(true)
-            setEnter('enter value and press "set"')
-        }
-    },[startValue, finishValue])
-
-    useEffect(() => {
-        let minString = localStorage.getItem('minValue')
-        if (minString) {
-            let minNumber = JSON.parse(minString)
-            setStartValue(minNumber)
-            setCounter(minNumber)
-        }
-        setDisabled(true)
-
-        let maxString = localStorage.getItem('maxValue')
-        if (maxString) {
-            let maxNumber = JSON.parse(maxString)
-            setFinishValue(maxNumber)
-        }
-
-    }, [])
-
-
-    const callBackHandlerForSet = () => {
-        setCounter(startValue)
-        setDisabled(true)
-        setEdit(false)
-        localStorage.setItem("minValue", startValue.toString())
-        localStorage.setItem("maxValue", finishValue.toString())
+//увеличивает значение в счетчике
+    const increaseValue = () => {
+        dispatch(increaseValueAC())
     }
 
-    const minInput = (min: number) => {
-        setStartValue(min)
-        setDisabled(false)
-
+//устанавливает значение счетчика
+    const setCounterValue = () => {
+            dispatch(setCounterValueAC(startValue))
+            dispatch(setSettingsMode(false))
     }
-
-    const maxInput = (max: number) => {
-        setFinishValue(max)
-        setDisabled(false)
-
-    }
-
 
     return (
-        <div className="App">
-            <Settings
-                startValue={startValue}
-                finishValue={finishValue}
-                minInput={minInput}
-                maxInput={maxInput}
-                callBackHandlerForSet={callBackHandlerForSet}
-                disabled={disabled}
-                error={error}
-                setError={setError}
-                setEdit={setEdit}
-            />
-            <Counter
-                value={counter}
-                disabled={disabled}
-                edit={edit}
-                startValue={startValue}
-                finishValue={finishValue}
-                counter={counter}
-                setCounter={setCounter}
-                setError={setError}
-                setEdit={setEdit}
-                error={error}
-                enter={enter}
-                setEnter={setEnter}
+        <div className="cover">
 
-            />
+            <div className={'settingsBlock'}>
+                <SettingsBlock/>
+                <Button title='set' callback={setCounterValue}/>
+            </div>
+            <div className={'counterBlock'}>
+                <CounterBlock/>
+                <Button title='inc' callback={increaseValue}/>
+                <Button title='reset' callback={setCounterValue}/>
+            </div>
 
         </div>
     );
